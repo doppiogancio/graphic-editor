a93884dd4e4d2b55a91e7b17f9b3fe3ce06b4f85
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2016-08-11 16:26:30 +0200

    Added GraphicPolygon

---
+++ lib/GraphicPolygon.php
@@ -0,0 +1,43 @@
+<?php
+
+class GraphicPolygon extends Graphic {
+
+	public function __construct( $settings ) {
+		parent::__construct( $settings );
+
+		if ( empty($this->settings['points']) ) {
+			throw new SettingRequiredException("Points are a required setting for GraphicPolygon!");
+		}
+	}
+
+	protected function _get_points() {
+
+		$points = array();
+
+		foreach ( $this->points as $point ) {
+			$points = array_merge($points, $point);
+		}
+
+		return $points;
+	}
+
+	public function draw() {
+
+
+		// create a blank image
+		$image = $this->graphic_editor->getImage();
+
+		// choose a color for the ellipse
+		$filler_color = imagecolorallocate($image, $this->filler_color[0], $this->filler_color[1], $this->filler_color[2]);
+
+		// draw the white ellipse
+		imagefilledpolygon($image, $this->_get_points(), count($this->points), $filler_color);
+
+		if ( isset($this->border['color']) ) {
+			// border color
+			$border_color = imagecolorallocate($image, $this->border['color'][0], $this->border['color'][1], $this->border['color'][2]);
+			imagepolygon($image, $this->_get_points(), count($this->points), $filler_color);
+		}
+
+	}
+}
