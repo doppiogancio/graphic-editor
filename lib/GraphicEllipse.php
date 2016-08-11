1eeb206c5d52fa5a152609f9e97f869cac5b4fd2
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2016-08-11 04:48:48 +0200

    Implemented method "draw" for Ellipse and Rectangle

--- lib/GraphicEllipse.php
+++ lib/GraphicEllipse.php
@@ -1,11 +1,25 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: fabriziogargiulo
- * Date: 09/08/16
- * Time: 20:30
- */
-class GraphicEllipse {
+class GraphicEllipse extends Graphic {
+
+	public function draw() {
+
+
+		// create a blank image
+		$image = $this->graphic_editor->getImage();
+
+		// choose a color for the ellipse
+		$col_ellipse = imagecolorallocate($image, $this->filler_color[0], $this->filler_color[1], $this->filler_color[2]);
+
+		// draw the white ellipse
+		imagefilledellipse($image, $this->left, $this->top, $this->width, $this->height, $col_ellipse);
+
+		if ( isset($this->border['color']) ) {
+			// border color
+			$border_color = imagecolorallocate($image, $this->border['color'][0], $this->border['color'][1], $this->border['color'][2]);
+			imageellipse ( $image, $this->left, $this->top, $this->width, $this->height, $border_color);
+		}
+
+	}
 
 }
--- lib/GraphicRectangle.php
+++ lib/GraphicRectangle.php
@@ -1,11 +1,18 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: fabriziogargiulo
- * Date: 09/08/16
- * Time: 20:30
- */
-class GraphicRectangle {
+class GraphicRectangle extends Graphic {
+	public function draw() {
 
+		$image = $this->graphic_editor->getImage();
+
+		$filler_color = imagecolorallocate($image, $this->filler_color[0], $this->filler_color[1], $this->filler_color[2]);
+
+		imagefilledrectangle($image, $this->left, $this->top, $this->left + $this->width, $this->top + $this->height, $filler_color);
+
+		if ( isset($this->border['color']) ) {
+			// border color
+			$border_color = imagecolorallocate($image, $this->border['color'][0], $this->border['color'][1], $this->border['color'][2]);
+			imagerectangle($image, $this->left, $this->top, $this->left + $this->width, $this->top + $this->height, $border_color);
+		}
+	}
 }
